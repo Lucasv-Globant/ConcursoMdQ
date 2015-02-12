af1f8f2a5f495c5571cd35dcc7204643f9a3ec71
Federico Gonzalez <federicoagonzalez@outlook.com>
2015-02-12 12:02:55 -0300

    Changed model for denormalization

--- Que Hacer? MdQ/Que Hacer? MdQ/Que Hacer? MdQ/Classes/DataManager/Activity.swift
+++ Que Hacer? MdQ/Que Hacer? MdQ/Que Hacer? MdQ/Classes/DataManager/Activity.swift
@@ -15,15 +15,23 @@ class Activity: NSManagedObject {
     @NSManaged var id: Int
     
     //Relationships:
-    @NSManaged var images: NSSet
-    @NSManaged var contacts: NSSet
     @NSManaged var feeds: NSSet
     @NSManaged var areas: NSSet
     @NSManaged var tags: NSSet
     @NSManaged var schedule: NSSet
-    @NSManaged var location: Location
 
-    //Attributes:
+    //Atributes
+    @NSManaged var locationLatitude: Float
+    @NSManaged var locationLongitude: Float
+    @NSManaged var locationStreetOrRoute: String
+    @NSManaged var locationHouseNumberingOrKm: String
+    @NSManaged var locationDetails: String  //in case of no specific data of street and number
+    @NSManaged var contactPhone1: String
+    @NSManaged var contactPhone2: String
+    @NSManaged var contactPhone3: String
+    @NSManaged var photoUrl1: String
+    @NSManaged var photoUrl2: String
+    @NSManaged var photoUrl3: String
     @NSManaged var name: String
     @NSManaged var desc: String
     @NSManaged var website: String //NSURL
@@ -38,6 +46,8 @@ class Activity: NSManagedObject {
     @NSManaged var paidParkingZone: Bool
     @NSManaged var sharingUrl: String
     @NSManaged var visible: Bool
+    @NSManaged var createdAt: NSDate
+    @NSManaged var updatedAt: NSDate
     
     //Inferred by other attributes for simplicity
 //    @NSManaged var occurrsOnce: Bool
