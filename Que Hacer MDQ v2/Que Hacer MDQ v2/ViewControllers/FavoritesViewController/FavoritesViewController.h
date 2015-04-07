2fd37c198d063580d902a8964ccbf84516d28425
Lucasv-Globant <lucas.violini@globant.com>
2015-04-07 14:54:18 -0300

    Added: Favorites View Controller (blank)

---
+++ Que Hacer MDQ v2/Que Hacer MDQ v2/ViewControllers/FavoritesViewController/FavoritesViewController.h
@@ -0,0 +1,13 @@
+//
+//  FavoritesViewController.h
+//  Que Hacer MDQ v2
+//
+//  Created by Lucas on 4/7/15.
+//  Copyright (c) 2015 Globant iOS MDQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FavoritesViewController : UIViewController
+
+@end
---
+++ Que Hacer MDQ v2/Que Hacer MDQ v2/ViewControllers/FavoritesViewController/FavoritesViewController.m
@@ -0,0 +1,37 @@
+//
+//  FavoritesViewController.m
+//  Que Hacer MDQ v2
+//
+//  Created by Lucas on 4/7/15.
+//  Copyright (c) 2015 Globant iOS MDQ. All rights reserved.
+//
+
+#import "FavoritesViewController.h"
+
+@interface FavoritesViewController ()
+
+@end
+
+@implementation FavoritesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
